f67035f6b8b3057c259f78150d50680fe22fe6d4
andrew <andrewmuhn@gmail.com>
2023-02-21 11:33:27 -0800

    added  timer to header

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,8 @@ var projectDateInputEl = $('#project-date-input');
 // handle displaying the time
 function displayTime() {
   // use dayjs to find date and print that
+  const now = dayjs().format('MMM DD, YYYY [at] HH:mm:ss a')
+  timeDisplayEl.text(now);
 }
 
 // Reads projects from local storage and returns array of project objects.
@@ -29,20 +31,20 @@ function printProjectData() {
 
   // get projects from localStorage
 
-  
+
   // loop through each project and create a row
   for (var i = 0; i < projects.length; i += 1) {
     // set local scoped variables
-    
+
     // get date/time for start of today
-    
+
 
     // Create row and columns for project
-    
+
 
     // Save the index of the project as a data-* attribute on the button. This
     // will be used when removing the project from the array.
-    
+
 
     // add class to row by comparing project date to today's date (and if it's due)
 
@@ -55,12 +57,12 @@ function printProjectData() {
 // Removes a project from local storage and prints the project data
 function handleDeleteProject() {
   // local scoped variables
-  
+
   // remove project from the array
-  
+
 
   // print projects
-  
+
 }
 
 // Adds a project to local storage and prints the project data
@@ -68,9 +70,9 @@ function handleProjectFormSubmit(event) {
   event.preventDefault();
 
   // read user input from the form
-  
 
-  var newProject = {  };
+
+  var newProject = {};
 
   // add project to local storage
   var projects = readProjectsFromStorage();
@@ -81,7 +83,7 @@ function handleProjectFormSubmit(event) {
   printProjectData();
 
   // clear the form inputs
-  
+
 }
 
 // add submit handler
